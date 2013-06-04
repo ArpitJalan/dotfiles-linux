daf5ddc2b896a7587d49bd9c8221b7d36c728f5f
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-06-04 11:48:18 -0300

    removing more unused things, putting path things in the right place, cleaning up zshrc

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,29 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# rbenv
+export RBENV_ROOT="${HOME}/.rbenv"
+if [ -d "${RBENV_ROOT}" ]; then
+  export PATH="${RBENV_ROOT}/bin:${PATH}"
+  eval "$(rbenv init -)"
+fi
+
+# ssh -X
+export NSS_SSL_CBC_RANDOM_IV=0
+
+# play framework
+export PLAY_PATH=/opt/play
+export PATH="${PLAY_PATH}:${PATH}"
+
+# node.js
+export NODE_HOME=/opt/node
+export PATH="${NODE_HOME}/bin:${PATH}"
+
+# java
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
+export PATH="${JAVA_HOME}/bin:${PATH}"
+
+# totvs ecm
+export ECM_HOME=${HOME}/.ecm
+export JBOSS_HOME=${ECM_HOME}/JBoss-7.1.1/
