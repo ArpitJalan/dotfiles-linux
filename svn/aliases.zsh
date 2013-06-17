c1c74c799a5dd898f523804d9c087bc2c947c0ea
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-06-17 08:22:27 -0300

    Merging pull-request from @lucasmerencia/dotfiles-linux

--- svn/aliases.zsh
+++ svn/aliases.zsh
@@ -2,10 +2,13 @@ alias svnu='svn up'
 alias svns='svn stat'
 alias svnl='svn log -l 10'
 alias svnc='svn commit'
-alias svndv='svn diff | vim -'
+alias svnr='svn revert'
 
 # shows only relevant changed files (for a java project)
 svs() {
   svn stat | grep -v "classpath" | grep -v "/bin" | grep -v ".settings" | grep -v ".project" | grep -v "target"
 }
 
+function svndv {
+  svn diff $@ | vim -
+}
