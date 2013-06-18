458e72014c5a92eba996efc850517a80fd8a8d25
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-06-18 09:04:18 -0300

    path

--- system/path.zsh
+++ system/path.zsh
@@ -5,22 +5,27 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 # rbenv
 export RBENV_ROOT="${HOME}/.rbenv"
 if [ -d "${RBENV_ROOT}" ]; then
-  export PATH="${RBENV_ROOT}/bin:${PATH}"
+  export PATH="${PATH}:${RBENV_ROOT}/bin"
   eval "$(rbenv init -)"
 fi
 
 # play framework
-export PLAY_PATH=/opt/play
-export PATH="${PLAY_PATH}:${PATH}"
+export PLAY_HOME=/opt/play
+export PATH="${PATH}:${PLAY_HOME}"
 
 # node.js
 export NODE_HOME=/opt/node
-export PATH="${NODE_HOME}/bin:${PATH}"
+export PATH="${PATH}:${NODE_HOME}/bin"
 
 # java
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
-export PATH="${JAVA_HOME}/bin:${PATH}"
+export PATH="${PATH}:${JAVA_HOME}/bin"
+
+# elixir
+export ELIXIR_HOME=/opt/elixir
+export PATH="${PATH}:${ELIXIR_HOME}/bin"
 
 # totvs ecm
 export ECM_HOME=/opt/byYou/SocialECM
 export JBOSS_HOME=${ECM_HOME}/JBoss-7.1.1/
+
