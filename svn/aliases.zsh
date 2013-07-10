e063d21f9ac77f2b13ea52d45c8711e0d19177e5
Lucas Merencia <lucas.merencia@gmail.com>
2013-07-10 09:28:23 -0300

    svn show changed files at revision

--- svn/aliases.zsh
+++ svn/aliases.zsh
@@ -1,10 +1,26 @@
 alias svnu='svn up'
+alias svns='svn stat'
+alias svnl='svn log -l 10'
+alias svnc='svn commit'
 alias svnr='svn revert'
-alias sst='svn stat'
-alias sstt='svn stat |  grep "^ \w"'
-alias slog='svn log -l 10'
-alias scm='svn commit'
 
-function sdf {
-  svn diff $@ | vim -
+# shows only relevant changed files (for a java project)
+svs() {
+  svn stat | grep -v "classpath" | grep -v "/bin" | grep -v ".settings" | grep -v ".project" | grep -v "target"
 }
+
+function svndv {
+		svn diff $@ | vim -
+}
+
+
+function svnls {
+	if [ -z "$1" ]; then
+		svs
+	else
+		revision=$1
+		previous=$((revision-1))
+		svn diff -r $revision:$previous --summarize
+	fi
+}
+
